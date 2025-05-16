bd8e0541182c945f522dc60c0297dfb1dcb0086a
dltnals317 <sumin4101@naver.com>
2025-05-17 04:11:39 +0900

    feat:배포 테스트

--- src/main/java/org/sopt/sopkathon/controller/UserController.java
+++ src/main/java/org/sopt/sopkathon/controller/UserController.java
@@ -1,10 +1,12 @@
 package org.sopt.sopkathon.controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
-    @GetMapping("/user")
+    @GetMapping("/test")
     public String check() {
         return "Server is running!";
     }
